7d30c4bdec15f079db798c3b0f1a8d059d759ede
Innes MacKenzie <innesm@gmail.com>
2016-04-24 22:25:03 +0100

    move chip select to 4 (gpio2)

--- scales/src/mcp3008.h
+++ scales/src/mcp3008.h
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <SPI.h>
 
+#define CHIPSELECT 4
+
 uint8_t reversebits(uint8_t b)
 {
   uint8_t r = 0;
@@ -16,11 +18,8 @@ void init_mcp3008_spi()
   //pinMode(15, OUTPUT);
   //digitalWrite(15, 1);
 
-  //todo - try a separate CS pin, so it can be pulled up to disable the ADC
-
-  pinMode(5, OUTPUT);
-  digitalWrite(5, 1);
-
+  pinMode(CHIPSELECT, OUTPUT);
+  digitalWrite(CHIPSELECT, 1);
   SPI.setHwCs(false);//true);
   SPI.begin();
   SPI.beginTransaction(SPISettings(SPI_CLOCK_DIV8, MSBFIRST, SPI_MODE0));
@@ -32,9 +31,9 @@ int read_analog(int channel)
 {
   uint8_t send_bytes[] = {static_cast<uint8_t>(0xC0 + (channel << 3)), 0, 0, 0};
   uint8_t recv_bytes[4];
-  digitalWrite(5, 0);
+  digitalWrite(CHIPSELECT, 0);
   SPI.transferBytes(send_bytes, recv_bytes, 4);
-  digitalWrite(5, 1);
+  digitalWrite(CHIPSELECT, 1);
   uint8_t b3 = reversebits(recv_bytes[3]);
   uint8_t b2 = reversebits(recv_bytes[2]);
   return (b3 << 8) + b2;
